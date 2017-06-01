89fdd9819d3d2633e211b13bb993d02a750bb086
Nmuta Jones <nmuta1@gmail.com>
2017-06-01 10:08:01 -0700

    initial commit

---
+++ file.js
@@ -0,0 +1,13 @@
+
+// url to get fruit image from:
+// https://www.iconfinder.com/data/icons/fruits/128/{{fruit}}.png
+
+//api to call to get all of the fruits :
+//https://salty-eyrie-86402.herokuapp.com/get_fruits.json
+
+$.ajax({
+  url: "http://fiddle.jshell.net/favicon.png",
+})
+  .done(function( data ) {
+
+  });
