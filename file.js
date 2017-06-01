d99fcbe30be1a01ff55e4f8204381c2b5e84cb23
Nmuta Jones <nmuta1@gmail.com>
2017-06-01 11:50:54 -0700

    popping in the fruits

--- file.js
+++ file.js
@@ -4,6 +4,18 @@
 
 //api to call to get all of the fruits :
 //https://salty-eyrie-86402.herokuapp.com/get_fruits.json
+var fruits = ["Tomato", "Apple", "Orange"];
+
+
+
+fruits.forEach(function(f){
+  $(".fruit_box").append(`
+    <div class="fruit_item">
+       <img src="https://www.iconfinder.com/data/icons/fruits/128/${f}.png">
+    </div>
+    `)
+})
+
 
 $.ajax({
   url: "http://fiddle.jshell.net/favicon.png",
