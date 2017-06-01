6834e3472030b9f657fb605e065b09311d60a2e0
Nmuta Jones <nmuta1@gmail.com>
2017-06-01 13:30:25 -0700

    DOM population, fruit images, names , etc

--- file.js
+++ file.js
@@ -8,18 +8,18 @@ var fruits = ["Tomato", "Apple", "Orange"];
 
 
 
-fruits.forEach(function(f){
-  $(".fruit_box").append(`
-    <div class="fruit_item">
-       <img src="https://www.iconfinder.com/data/icons/fruits/128/${f}.png">
-    </div>
-    `)
-})
 
 
 $.ajax({
-  url: "http://fiddle.jshell.net/favicon.png",
+  url: "https://salty-eyrie-86402.herokuapp.com/get_fruits.json",
 })
   .done(function( data ) {
-
+    data.data.forEach(function(f){
+      $(".fruit_box").append(`
+        <div class="fruit_item">
+           <img src="https://www.iconfinder.com/data/icons/fruits/128/${f.name}.png">
+           <div style="text-align:center"> ${f.name} </div>
+        </div>
+        `)
+    })
   });
